84091e960a8f5e9ebc874ac93d1ab359b72fcc9c
peacepeacecreation <peacepeacecreation@gmail.com>
2025-05-13 16:05:06 +0300

    ADD openai sdk

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -3,7 +3,8 @@ import {
   extractReasoningMiddleware,
   wrapLanguageModel,
 } from 'ai';
-import { xai } from '@ai-sdk/xai';
+import { createOpenAI } from '@ai-sdk/openai';
+
 import { isTestEnvironment } from '../constants';
 import {
   artifactModel,
@@ -11,6 +12,11 @@ import {
   reasoningModel,
   titleModel,
 } from './models.test';
+
+const openai = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY || '',
+  compatibility: 'strict', // strict mode, enable when using the OpenAI API
+});
 
 export const myProvider = isTestEnvironment
   ? customProvider({
@@ -23,15 +29,15 @@ export const myProvider = isTestEnvironment
     })
   : customProvider({
       languageModels: {
-        'chat-model': xai('grok-2-vision-1212'),
+        'chat-model': openai('gpt-4o-mini'),
         'chat-model-reasoning': wrapLanguageModel({
-          model: xai('grok-3-mini-beta'),
-          middleware: extractReasoningMiddleware({ tagName: 'think' }),
+            model: openai('gpt-4o-mini'),
+            middleware: extractReasoningMiddleware({ tagName: 'think' }),
         }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
+        'title-model': openai('gpt-4o-mini'),
+        'artifact-model': openai('gpt-4o-mini'),
       },
       imageModels: {
-        'small-model': xai.image('grok-2-image'),
+        'small-model': openai.image('dall-e-3'),
       },
     });
